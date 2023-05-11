29f0889f885cb1202a19ab82c0ed986ddbaf188c
Rafael Rodrigues <rafaelrodripaz@gmail.com>
2023-05-10 21:03:18 -0300

    Update jspyrate.go

--- jspyrate.go
+++ jspyrate.go
@@ -1,190 +1,118 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
 	"io/ioutil"
-	"net/url"
+	"net/http"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"sync"
-
-	"github.com/jquery/esprima"
-	"github.com/urfave/cli/v2"
 )
 
-func main() {
-	app := &cli.App{
-		Name:  "js_analyzer",
-		Usage: "Analyze JavaScript files to extract URLs, endpoints, and secrets",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "input",
-				Aliases:  []string{"i"},
-				Usage:    "Input file containing a list of JavaScript files",
-				Required: true,
-			},
-			&cli.StringFlag{
-				Name:    "regex",
-				Aliases: []string{"r"},
-				Usage:   "Optional regex list file for hardcoded secrets",
-			},
-		},
-		Action: func(c *cli.Context) error {
-			input := c.String("input")
-			regexFile := c.String("regex")
+func extractEndpoints(data string, regexes []string) []string {
+	var endpoints []string
 
-			var regexList []string
-			if regexFile != "" {
-				regexList = loadRegexList(regexFile)
-			}
-
-			jsFiles := loadJSFiles(input)
-			var wg sync.WaitGroup
-			sem := make(chan bool, 10)
-
-			for _, file := range jsFiles {
-				wg.Add(1)
-				sem <- true
-				go func(filePath string) {
-					defer func() {
-						<-sem
-						wg.Done()
-					}()
-					analyzeFile(filePath, regexList)
-				}(file)
-			}
-			wg.Wait()
-			return nil
-		},
+	for _, regexStr := range regexes {
+		re := regexp.MustCompile(regexStr)
+		matches := re.FindAllString(data, -1)
+		endpoints = append(endpoints, matches...)
 	}
 
-	err := app.Run(os.Args)
+	return endpoints
+}
+
+func loadRegexes(wordlistPath string) ([]string, error) {
+	data, err := ioutil.ReadFile(wordlistPath)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
+	}
+
+	lines := strings.Split(string(data), "\n")
+	return lines, nil
+}
+
+func downloadFile(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+func processURL(url string, outputPath string, regexes []string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	data, err := downloadFile(url)
+	if err != nil {
+		fmt.Printf("Error downloading file: %s\n", url)
+		return
+	}
+
+	content := data
+	endpoints := extractEndpoints(content, regexes)
+
+	if len(endpoints) > 0 {
+		output := strings.Join(endpoints, "\n")
+
+		filename := filepath.Base(url) + ".txt"
+		outFile := filepath.Join(outputPath, filename)
+		err := ioutil.WriteFile(outFile, []byte(output), 0644)
+		if err != nil {
+			fmt.Printf("Error writing to file: %s\n", outFile)
+		}
+	}
+}
+
+func main() {
+	var wordlistPath string
+	var inputURL string
+	var outputPath string
+	var threads int
+
+	flag.StringVar(&wordlistPath, "wordlist", "", "Path to the wordlist containing regexes for hardcoded secrets")
+	flag.StringVar(&inputURL, "url", "", "A single URL of a JavaScript file")
+	flag.StringVar(&outputPath, "output", "", "Path to the directory to save the output files")
+	flag.IntVar(&threads, "threads", 10, "Number of concurrent threads")
+
+	flag.Parse()
+
+	if outputPath == "" {
+		fmt.Println("The output directory must be specified")
 		os.Exit(1)
 	}
-}
 
-func loadJSFiles(filePath string) []string {
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Printf("Error opening file %s: %v\n", filePath, err)
-		return nil
-	}
-	defer file.Close()
+	var regexes []string
+	var err error
 
-	var files []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		files = append(files, strings.TrimSpace(scanner.Text()))
-	}
-
-	return files
-}
-
-func loadRegexList(filePath string) []string {
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Printf("Error opening file %s: %v\n", filePath, err)
-		return nil
-	}
-	defer file.Close()
-
-	var regexList []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		regexList = append(regexList, strings.TrimSpace(scanner.Text()))
-	}
-
-	return regexList
-}
-
-func analyzeFile(filePath string, regexList []string) {
-	fileContent, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		fmt.Printf("Error reading file %s: %v\n", filePath, err)
-		return
-	}
-
-	content := string(fileContent)
-
-	urls := extractURLs(content)
-	endpoints := extractEndpoints(content)
-
-	// Call the parseEndpoints function to extract more complex endpoints
-	complexEndpoints := parseEndpoints(content)
-	endpoints = append(endpoints, complexEndpoints...)
-
-	secrets := extractSecrets(content, regexList)
-
-	outputFile := fmt.Sprintf("%s_output.txt", strings.TrimSuffix(filePath, ".js"))
-	output, err := os.Create(outputFile)
-	if err != nil {
-		fmt.Printf("Error creating output file %s: %v\n", outputFile, err)
-		return
-	}
-	defer output.Close()
-
-	writer := bufio.NewWriter(output)
-
-	writer.WriteString("URLs:\n")
-	for _, u := range urls {
-		writer.WriteString(u + "\n")
-	}
-
-	writer.WriteString("\nEndpoints:\n")
-	for _, e := range endpoints {
-		writer.WriteString(e + "\n")
-	}
-
-	if len(regexList) > 0 {
-		writer.WriteString("\nSecrets:\n")
-		for _, s := range secrets {
-			writer.WriteString(s + "\n")
+	if wordlistPath != "" {
+		regexes, err = loadRegexes(wordlistPath)
+		if err != nil {
+			fmt.Printf("Error reading wordlist: %s\n", wordlistPath)
+			os.Exit(1)
 		}
 	}
 
-	writer.Flush()
-}
+	var wg sync.WaitGroup
+	sem := make(chan bool, threads)
 
-func extractURLs(content string) []string {
-	urlRegex := regexp.MustCompile(`https?://[^/\s]+/\S*`)
-	return urlRegex.FindAllString(content, -1)
-}
-
-func extractEndpoints(content string) []string {
-	endpointRegex := regexp.MustCompile(`(["'])\/[\w-]+(?:\/[\w-]+)*\1`)
-	matches := endpointRegex.FindAllString(content, -1)
-
-	var endpoints []string
-	for _, match := range matches {
-		endpoints = append(endpoints, strings.Trim(match, "\"'"))
+	if inputURL != "" {
+		wg.Add(1)
+		sem <- true
+		go func(url string) {
+			processURL(url, outputPath, regexes, &wg)
+			<-sem
+		}(inputURL)
 	}
 
-	return endpoints
+	wg.Wait()
 }
-
-func parseEndpoints(content string) []string {
-	var endpoints []string
-	ast, err := esprima.ParseModule(content)
-	if err == nil {
-		// Implement your logic to parse the AST for complex endpoints
-	}
-
-	return endpoints
-}
-
-func extractSecrets(content string, regexList []string) []string {
-	var secrets []string
-
-	for _, regex := range regexList {
-		re := regexp.MustCompile(regex)
-		matches := re.FindAllString(content, -1)
-		secrets = append(secrets, matches...)
-	}
-
-	return secrets
-}
-
