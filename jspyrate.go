a6ff2a78b05a31a59bdd5056d6ad95eb09e9528f
Rafael Rodrigues <rafaelrodripaz@gmail.com>
2023-05-10 21:07:33 -0300

    Update jspyrate.go

--- jspyrate.go
+++ jspyrate.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -12,107 +14,158 @@ import (
 	"sync"
 )
 
-func extractEndpoints(data string, regexes []string) []string {
-	var endpoints []string
+var (
+	urlsFile       string
+	secretsFile    string
+	outputDir      string
+	downloadedPath string
+)
 
-	for _, regexStr := range regexes {
-		re := regexp.MustCompile(regexStr)
-		matches := re.FindAllString(data, -1)
-		endpoints = append(endpoints, matches...)
-	}
-
-	return endpoints
-}
-
-func loadRegexes(wordlistPath string) ([]string, error) {
-	data, err := ioutil.ReadFile(wordlistPath)
-	if err != nil {
-		return nil, err
-	}
-
-	lines := strings.Split(string(data), "\n")
-	return lines, nil
-}
-
-func downloadFile(url string) (string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
-}
-
-func processURL(url string, outputPath string, regexes []string, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	data, err := downloadFile(url)
-	if err != nil {
-		fmt.Printf("Error downloading file: %s\n", url)
-		return
-	}
-
-	content := data
-	endpoints := extractEndpoints(content, regexes)
-
-	if len(endpoints) > 0 {
-		output := strings.Join(endpoints, "\n")
-
-		filename := filepath.Base(url) + ".txt"
-		outFile := filepath.Join(outputPath, filename)
-		err := ioutil.WriteFile(outFile, []byte(output), 0644)
-		if err != nil {
-			fmt.Printf("Error writing to file: %s\n", outFile)
-		}
-	}
+func init() {
+	flag.StringVar(&urlsFile, "urls", "", "Path to the file containing .js URLs")
+	flag.StringVar(&secretsFile, "secrets", "", "Path to the file containing regex patterns for hardcoded secrets (optional)")
+	flag.StringVar(&outputDir, "output", "output", "Path to the output directory")
+	flag.StringVar(&downloadedPath, "download", "downloaded_js", "Path to the directory where the downloaded .js files will be stored")
 }
 
 func main() {
-	var wordlistPath string
-	var inputURL string
-	var outputPath string
-	var threads int
-
-	flag.StringVar(&wordlistPath, "wordlist", "", "Path to the wordlist containing regexes for hardcoded secrets")
-	flag.StringVar(&inputURL, "url", "", "A single URL of a JavaScript file")
-	flag.StringVar(&outputPath, "output", "", "Path to the directory to save the output files")
-	flag.IntVar(&threads, "threads", 10, "Number of concurrent threads")
-
 	flag.Parse()
 
-	if outputPath == "" {
-		fmt.Println("The output directory must be specified")
+	if urlsFile == "" {
+		fmt.Println("Error: missing -urls flag")
+		flag.Usage()
 		os.Exit(1)
 	}
 
-	var regexes []string
-	var err error
+	urls, err := readLines(urlsFile)
+	if err != nil {
+		fmt.Printf("Error reading URLs file: %v\n", err)
+		os.Exit(1)
+	}
 
-	if wordlistPath != "" {
-		regexes, err = loadRegexes(wordlistPath)
+	secretsRegex := []*regexp.Regexp{}
+	if secretsFile != "" {
+		secretsPatterns, err := readLines(secretsFile)
 		if err != nil {
-			fmt.Printf("Error reading wordlist: %s\n", wordlistPath)
+			fmt.Printf("Error reading secrets file: %v\n", err)
 			os.Exit(1)
+		}
+		for _, pattern := range secretsPatterns {
+			secretsRegex = append(secretsRegex, regexp.MustCompile(pattern))
 		}
 	}
 
-	var wg sync.WaitGroup
-	sem := make(chan bool, threads)
+	os.MkdirAll(downloadedPath, os.ModePerm)
+	os.MkdirAll(outputDir, os.ModePerm)
 
-	if inputURL != "" {
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, 10) // Limit concurrency to 10 threads
+
+	for _, url := range urls {
 		wg.Add(1)
-		sem <- true
+		sem <- struct{}{} // Acquire a token
+
 		go func(url string) {
-			processURL(url, outputPath, regexes, &wg)
-			<-sem
-		}(inputURL)
+			defer func() {
+				<-sem // Release the token
+				wg.Done()
+			}()
+
+			filename := filepath.Base(url)
+			downloadJS(url, filepath.Join(downloadedPath, filename))
+			endpoints, secrets := analyzeJS(filepath.Join(downloadedPath, filename), secretsRegex)
+
+			writeResults(filepath.Join(outputDir, filename+".txt"), endpoints, secrets)
+		}(url)
 	}
 
 	wg.Wait()
 }
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func downloadJS(url, path string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error downloading %s: %v\n", url, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(path)
+	if err != nil {
+		fmt.Printf("Error creating file %s: %v\n", path, err)
+		return
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Printf("Error saving file %s: %v\n", path, err)
+		return
+	}
+}
+
+func analyzeJS(path string, secretsRegex []*regexp.Regexp) (endpoints, secrets []string) {
+	data,	err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Error reading file %s: %v\n", path, err)
+		return
+	}
+
+	content := string(data)
+
+	endpointRegex := regexp.MustCompile(`\b(?:https?://|/)[^"'\\]*(?:\.json|\.php|\.aspx?|\.jsp|\.do|\.cgi|\.cfm|\.action)[^"'\\]*\b`)
+	urlRegex := regexp.MustCompile(`\bhttps?://[^\s"'\\]+(?:\.html|\.htm|\.js|\.css|\.xml|\.rss|\.atom|\.txt)[^"'\\]*\b`)
+
+	endpointMatches := endpointRegex.FindAllString(content, -1)
+	urlMatches := urlRegex.FindAllString(content, -1)
+
+	endpoints = append(endpoints, endpointMatches...)
+	endpoints = append(endpoints, urlMatches...)
+
+	for _, re := range secretsRegex {
+		secretsMatches := re.FindAllString(content, -1)
+		secrets = append(secrets, secretsMatches...)
+	}
+
+	return
+}
+
+func writeResults(path string, endpoints, secrets []string) {
+	file, err := os.Create(path)
+	if err != nil {
+		fmt.Printf("Error creating output file %s: %v\n", path, err)
+		return
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	for _, endpoint := range endpoints {
+		writer.WriteString("Endpoint: " + endpoint + "\n")
+	}
+
+	if len(secrets) > 0 {
+		writer.WriteString("\nHardcoded secrets:\n")
+		for _, secret := range secrets {
+			writer.WriteString(secret + "\n")
+		}
+	}
+}
+
+
